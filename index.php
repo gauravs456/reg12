544aa8c4f1116f9e37736e1edc53d26de84db1ba
gauravs456 <varun544v@gaurav.us-central1-a.c.united-night-266008.internal>
2020-01-29 06:09:12 +0000

    previous

--- index.php
+++ index.php
@@ -54,11 +54,6 @@
         });
     </script>
 
-
-
-
-
-
     <script>
 
         $(document).ready(function(){
@@ -326,7 +321,7 @@
                 <input type="password" class="input" id="Passlogin" name="Passlogin"  required >
             </div>
             <div class="inputfield">
-                <input type="Button   id="loginbtn" value="Login" name="loginbtn" class="btn">
+                <input type="Button"   id="loginbtn" value="Login" name="loginbtn" class="btn">
             </div>
         </form>
 
