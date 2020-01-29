b91a9920b776a7404b0b3a50b8b28304a1145ac4
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-29 18:41:04 +0530

    prenent log

--- index.php
+++ index.php
@@ -87,6 +87,17 @@
             });
         });
     </script>
+	
+	
+	  <script type="text/javascript"> 
+        function preventBack() { 
+            window.history.forward();  
+        } 
+          
+        setTimeout("preventBack()", 0); 
+          
+        window.onunload = function () { null }; 
+    </script>
 
 
 	
