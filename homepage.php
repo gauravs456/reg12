21add21dc885508b01fd869f08b6ef21b17b35e3
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-30 22:04:30 +0530

    hide

--- homepage.php
+++ homepage.php
@@ -13,7 +13,7 @@ include('welcome.php');?>
 
 <body>
 <!-- Modal -->
-<div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- <div class="modal fade" id="studentaddmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -55,7 +55,7 @@ include('welcome.php');?>
     </div>
   </div>
 </div>
-
+ -->
 
 
 
