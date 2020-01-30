b1bdcf3c88bf90d1b435e38f5c43089e033797f4
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-30 15:00:09 +0530

    session start

--- logout.php
+++ logout.php
@@ -9,14 +9,14 @@ $conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+session_start();
 session_destroy();
 
 
-      header("Cache-Control: private, must-revalidate, max-age=0");
-      header("Pragma: no-cache");
-      header("Expires: Fri, 4 Jun 2010 12:00:00 GMT");
+header("Cache-Control: private, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: Fri, 4 Jun 2010 12:00:00 GMT");
 header("Location:http://34.68.249.249/reg12/index.php");
 $conn->close();
 
 ?>
-
