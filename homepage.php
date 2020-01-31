07ed90fd2d886dee7d737ff24f5ffda945a6231c
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-31 08:54:36 +0530

    title changed

--- homepage.php
+++ homepage.php
@@ -6,7 +6,7 @@ include('welcome.php');?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> PHP CRUD with Bootstrap Modal </title>
+    <title> Show Details </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.bootstrap4.min.css">
     </head>
