b38fc65ebd0a8b900a03ef548e4037657f4ca15e
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-31 14:00:14 +0530

    changes in getting

--- updatedata.php
+++ updatedata.php
@@ -8,11 +8,11 @@ $conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
 if(!isset($_POST['updatedata']))
 {
     $id = $_POST['id'];
-
-    $name = $_POST['name'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
+    
+     $name=mysqli_real_escape_string($conn,$_POST['name']);
+     $gender=mysqli_real_escape_string($conn,$_POST['gender']);
+ $email=mysqli_real_escape_string($conn,$_POST['email']);
+    $phone=mysqli_real_escape_string($conn,$_POST['phone']);
 
     $query = "UPDATE registration SET name='$name', gender='$gender', email='$email', phone=' $phone' WHERE id='$id'  ";
     $query_run = mysqli_query($conn, $query);
