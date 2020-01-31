695c6a58d0be2dd44be68afdd341d6ccb9364cbf
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-01-31 11:28:17 +0530

    shaking

--- index.php
+++ index.php
@@ -142,7 +142,7 @@
         }
     }
 
-    var addError = function() { element.classList.add('error'); };
+    var addError = function() { element.classList.add('wrapp'); };
 
     function emailval(emailid){
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
