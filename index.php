ed527850e13c256e8e5b62981cf473f5e432ee30
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 16:30:00 +0530

    yup

--- index.php
+++ index.php
@@ -10,17 +10,18 @@ if(isset($_SESSION['id']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
-
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"> </script>
 <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"> </script>
+
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Registration Form</title>
@@ -110,17 +111,10 @@ if(isset($_SESSION['id']))
             $('#course').val(data[3]);
             $('#contact').val(data[4]);
         });
-    });
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    });    
     </script>
+    
+    
     <script>
         $(document).ready(function(){
             $('input').blur(function(){
