2fb72659c7f9562e1f59ca2e508074cb4a3acd86
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 18:14:03 +0530

    revert

--- passwordreset.php
+++ passwordreset.php
@@ -20,33 +20,6 @@ if(isset($_POST['Email'])) {
         $encode = md5($email);
         $sub = "BINGO";
 
-        
-        
-        $mail = new PHPMailer;
-        $mail->isSMTP();
-        $mail->SMTPDebug = 2;
-        $mail->Host = '	smtp.mailtrap.io';
-        $mail->Port =  2525;
-        $mail->SMTPAuth = true;
-        $mail->Username = '	d8ac7f95d97bbc';
-        $mail->Password = '	1c06beb3a72dba';
-        $mail->setFrom('test@hostinger-tutorials.com', 'Your Name');
-        $mail->addReplyTo('test@hostinger-tutorials.com', 'Your Name');
-        $mail->addAddress('gevece5071@riv3r.net', 'Receiver Name');
-        $mail->Subject = 'Testing PHPMailer';
-        $mail->msgHTML(file_get_contents('message.html'), __DIR__);
-        $mail->Body = 'This is a plain text message body';
-//$mail->addAttachment('test.txt');
-          echo 'The email message was sent.';
-        if (!$mail->send()) {
-            echo 'Mailer Error: ' . $mail->ErrorInfo;
-        } else {
-            echo 'The email message was sent.';
-        }
-
-
-
-
     }
     else{
         echo("Not Registered");
