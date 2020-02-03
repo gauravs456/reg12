028a1b7ca1f83ab737d3b78a22f26a243a9af027
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 18:06:41 +0530

    qwe

--- passwordreset.php
+++ passwordreset.php
@@ -1,5 +1,5 @@
 <?php
-
+use PHPMailer\PHPMailer\PHPMailer;
 $db_name="rtds1";
 $mysql_username="puppy";
 $mysql_password="Puppy@123";
@@ -20,31 +20,31 @@ if(isset($_POST['Email'])) {
         $encode = md5($email);
         $sub = "BINGO";
 
-  use PHPMailer\PHPMailer\PHPMailer;
-require 'vendor/autoload.php';
-$mail = new PHPMailer;
-$mail->isSMTP();
-$mail->SMTPDebug = 2;
-$mail->Host = '	smtp.mailtrap.io';
-$mail->Port =  2525;
-$mail->SMTPAuth = true;
-$mail->Username = '	d8ac7f95d97bbc';
-$mail->Password = '	1c06beb3a72dba';
-$mail->setFrom('test@hostinger-tutorials.com', 'Your Name');
-$mail->addReplyTo('test@hostinger-tutorials.com', 'Your Name');
-$mail->addAddress('gevece5071@riv3r.net', 'Receiver Name');
-$mail->Subject = 'Testing PHPMailer';
-$mail->msgHTML(file_get_contents('message.html'), __DIR__);
-$mail->Body = 'This is a plain text message body';
+        
+        require 'vendor/autoload.php';
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->SMTPDebug = 2;
+        $mail->Host = '	smtp.mailtrap.io';
+        $mail->Port =  2525;
+        $mail->SMTPAuth = true;
+        $mail->Username = '	d8ac7f95d97bbc';
+        $mail->Password = '	1c06beb3a72dba';
+        $mail->setFrom('test@hostinger-tutorials.com', 'Your Name');
+        $mail->addReplyTo('test@hostinger-tutorials.com', 'Your Name');
+        $mail->addAddress('gevece5071@riv3r.net', 'Receiver Name');
+        $mail->Subject = 'Testing PHPMailer';
+        $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+        $mail->Body = 'This is a plain text message body';
 //$mail->addAttachment('test.txt');
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'The email message was sent.';
-}
+        if (!$mail->send()) {
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'The email message was sent.';
+        }
 
-        
-        
+
+
 
     }
     else{
@@ -52,7 +52,7 @@ if (!$mail->send()) {
     }
 }
 
-    $conn->close();
+$conn->close();
 
 
-    ?>
+?>
