f22480c6dfe37cee8e16b988c1fcd7d71d53cfa4
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 16:58:41 +0530

    passwordreset

---
+++ passwordreset.php
@@ -0,0 +1,36 @@
+<?php
+
+$db_name="rtds1";
+$mysql_username="puppy";
+$mysql_password="Puppy@123";
+$server_name="34.68.249.249";
+
+
+$conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+if(isset($_POST['Email'])){
+    $usr=mysqli_real_escape_string($conn,$_POST['Email']);
+    $email = "SELECT * FROM registration WHERE Email='$usr'";
+    $flagmail = mysqli_query($conn, $email);
+    if(mysqli_num_rows($flagmail)>0){
+      $email=md5($email);
+      echo ($email);
+    }
+    else{
+        echo ("Sorry You  are not registered");
+
+        }
+
+
+
+
+}
+
+$conn->close();
+
+
+?>
