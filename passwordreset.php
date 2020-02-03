1930f1919851d8ffd365454932b5abc80122adde
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 18:48:33 +0530

    sdf

--- passwordreset.php
+++ passwordreset.php
@@ -17,10 +17,26 @@ if(isset($_POST['Email'])) {
     $flagmail = mysqli_query($conn, $email);
     if (mysqli_num_rows($flagmail) > 0) {
         $encode = md5($email);
-     
-
-          $sub = "BINGO";
-        echo($sub);
+    $sub = "BINGO";
+          $to = "gevece5071@riv3r.net";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:abc@somedomain.com \r\n";
+         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+       
     }
     else{
         echo("Not Registered");
