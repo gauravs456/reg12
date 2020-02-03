3d6e61df1eeae4543a92a2b13a6e3d0f522277c9
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 18:52:06 +0530

    sdf

--- passwordreset.php
+++ passwordreset.php
@@ -18,24 +18,11 @@ if(isset($_POST['Email'])) {
     if (mysqli_num_rows($flagmail) > 0) {
         $encode = md5($email);
     $sub = "BINGO";
-          $to = "gauravsharma121998@gmail.com";
-         $subject = "This is subject";
-         
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
-         
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+    if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
        
     }
     else{
