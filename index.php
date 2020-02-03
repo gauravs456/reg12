343cc8771e7eced2a6ded021fc7e78a8587f1253
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 16:27:29 +0530

    yiipi

--- index.php
+++ index.php
@@ -345,54 +345,7 @@ if(isset($_SESSION['id']))
         </form>
         
         
-        <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"> Edit Student Data </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <form action="http://34.68.249.249/reg12/updatedata.php" method="POST">
-
-                <div class="modal-body">
-
-                    <input type="hidden" name="id" id="id">
-
-                    <div class="form-group">
-                        <label> Name </label>
-                        <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name Name">
-                    </div>
-
-                    <div class="form-group">
-                        <label> Gender </label>
-                        <input type="text" name="gender" id="gender" class="form-control" placeholder="Enter Gender">
-                    </div>
-
-                    <div class="form-group">
-                        <label> Email </label>
-                        <input type="text" name="email" id="email" class="form-control" placeholder=" Enter Email Id">
-                    </div>
-
-                    <div class="form-group">
-                        <label> Phone Number </label>
-                        <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter Phone Number">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
-                </div>
-            </form>
-
-        </div>
-    </div>
-</div>
-
-        
-        
+    
         
         
         <form id="signup" action = "http://34.68.249.249/reg12/Register.php" method="post" name = "myForm" onsubmit = "return(validate());">
@@ -442,5 +395,57 @@ if(isset($_SESSION['id']))
         </div>
     </div>
 </div>
+        
+            <div class="modal fade" id="editmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"> Edit Student Data </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <form action="http://34.68.249.249/reg12/updatedata.php" method="POST">
+
+                <div class="modal-body">
+
+                    <input type="hidden" name="id" id="id">
+
+                    <div class="form-group">
+                        <label> Name </label>
+                        <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name Name">
+                    </div>
+
+                    <div class="form-group">
+                        <label> Gender </label>
+                        <input type="text" name="gender" id="gender" class="form-control" placeholder="Enter Gender">
+                    </div>
+
+                    <div class="form-group">
+                        <label> Email </label>
+                        <input type="text" name="email" id="email" class="form-control" placeholder=" Enter Email Id">
+                    </div>
+
+                    <div class="form-group">
+                        <label> Phone Number </label>
+                        <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter Phone Number">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" name="updatedata" class="btn btn-primary">Update Data</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+        
+        
+        
+        
+        
 </body>
 </html>
