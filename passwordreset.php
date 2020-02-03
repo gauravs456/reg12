083460e174e4cea70e4981ee63fe553ccbf4ddcb
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 19:09:38 +0530

    revert

--- passwordreset.php
+++ passwordreset.php
@@ -1,34 +1,32 @@
 <?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
-  require 'PHPMailer-master/src/Exception.php';
-  require 'PHPMailer-master/src/PHPMailer.php';
-  require 'PHPMailer-master/src/SMTP.php';
-require 'vendor/autoload.php';
-  $mail = new PHPMailer();
-  $mail->IsSMTP();
+$db_name="rtds1";
+$mysql_username="puppy";
+$mysql_password="Puppy@123";
+$server_name="34.68.249.249";
 
-  $mail->SMTPDebug  = 0;  
-  $mail->SMTPAuth   = TRUE;
-  $mail->SMTPSecure = "tls";
-  $mail->Port       =  2525;
-  $mail->Host       = "	smtp.mailtrap.io";
-  $mail->Username   = "d8ac7f95d97bbc";
-  $mail->Password   = "1c06beb3a72dba";
 
-  $mail->IsHTML(true);
-  $mail->AddAddress("gevece5071@riv3r.net", "recipient-name");
-  $mail->SetFrom("your-email@gmail.com", "set-from-name");
-  $mail->AddReplyTo("reply-to-email", "reply-to-name");
-  $mail->AddCC("cc-recipient-email", "cc-recipient-name");
-  $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-  $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+$conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-  $mail->MsgHTML($content); 
-  if(!$mail->Send()) {
-    echo "Error while sending Email.";
-    var_dump($mail);
-  } else {
-    echo "Email sent successfully";
-  }
+
+if(isset($_POST['Email'])) {
+    $usr = mysqli_real_escape_string($conn, $_POST['Email']);
+    $email = "SELECT * FROM registration WHERE Email='$usr'";
+    $flagmail = mysqli_query($conn, $email);
+    if (mysqli_num_rows($flagmail) > 0) {
+        $encode = md5($email);
+        $sub = "BINGO";
+        echo($sub);
+
+    }
+    else{
+        echo("Not Registered");
+    }
+}
+
+$conn->close();
+
+
 ?>
