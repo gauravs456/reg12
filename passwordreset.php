881d0a9c6d8b13d1d4bf17bdc6eb105089a6d0fa
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-03 19:00:03 +0530

    fgh

--- passwordreset.php
+++ passwordreset.php
@@ -1,10 +1,5 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
 
 
 $db_name="rtds1";
