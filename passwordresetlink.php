7da0a6775de984f0266e94e56d8dc79539f83014
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-04 12:50:22 +0530

    ret

--- passwordresetlink.php
+++ passwordresetlink.php
@@ -1,30 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="styles.css">
+
+
+
+
 </head>
 
-<link rel="stylesheet" href="styles.css">
+<style>
+    #up{
+        font-size: 20px;
+    }
+
+</style>
+
 
 <body>
 
 
 
+
+<div class="wrapper" id="contain">
 <div class="form">
-    <form id="login_form"  name="login_form" method="post" onsubmit = "return(validate());" >
+    <h2 id="up" class="title">Welcome Back ! gaurav <h2>
+    <form id="login_form" name="login_form" method="post" onsubmit="return(validate());">
         <div class="inputfield">
             <label>Your New Password</label>
-            <input type="password" class="input" id="Emlogin" autocomplete="off" name="Emlogin"  onkeyup="emailvallogin(this.value);">
+            <input type="password" class="input" id="Emlogin" autocomplete="off" name="Emlogin"
+                   onkeyup="emailvallogin(this.value);">
         </div>
 
         <div class="inputfield">
-            <label> Confirm new  Password</label>
+            <label> Confirm new Password</label>
             <input type="password" class="input" id="Passlogin" name="Passlogin">
         </div>
         <div class="inputfield">
-            <input type="submit"  id="loginbtn" value="Reset Password" name="loginbtn" class="btn">
+            <input type="submit" id="loginbtn" value="Reset Password" name="loginbtn" class="btn">
         </div>
 
     </form>
+</div>
+</div>
 
 </body>
 </html>
