d72e464325e0a842469ae74c952625d89a65d092
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-04 10:58:29 +0530

    ert

--- passwordreset.php
+++ passwordreset.php
@@ -7,7 +7,7 @@ $mysql_password="Puppy@123";
 $server_name="34.68.249.249";
 $conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
 
-if(isset($_POST['Name'])){
+if(isset($_POST['resetemail'])){
     $email=mysqli_real_escape_string($conn,$_POST['resetemail']);
 }
  
