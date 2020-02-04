7d5640567e1ce48fea6aa20e8f75bb2bf2dd5c54
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-04 10:22:19 +0530

    tutorial

--- passwordreset.php
+++ passwordreset.php
@@ -1,32 +1,31 @@
-<?php
-$db_name="rtds1";
-$mysql_username="puppy";
-$mysql_password="Puppy@123";
-$server_name="34.68.249.249";
-
-
-$conn = new mysqli($server_name, $mysql_username, $mysql_password, $db_name);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-
-if(isset($_POST['Email'])) {
-    $usr = mysqli_real_escape_string($conn, $_POST['Email']);
-    $email = "SELECT * FROM registration WHERE Email='$usr'";
-    $flagmail = mysqli_query($conn, $email);
-    if (mysqli_num_rows($flagmail) > 0) {
-        $encode = md5($email);
-        $sub = "BINGO";
-        echo($sub);
-
-    }
-    else{
-        echo("Not Registered");
-    }
-}
-
-$conn->close();
-
-
-?>
+<html>
+   
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+      
+      <?php
+         $to = "gauravsharma121998@gmail.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:abc@somedomain.com \r\n";
+         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
+      
+   </body>
+</html>
