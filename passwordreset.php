9126967d70eacddda11a6f0bc20dc3b565309ac1
gauravs456 <32729223+gauravs456@users.noreply.github.com>
2020-02-04 11:20:03 +0530

    dsf

--- passwordreset.php
+++ passwordreset.php
@@ -10,35 +10,36 @@ $email=($_POST['resetemail']);
  $query = "SELECT * FROM registration WHERE Email='$email'";
 $run=msqli_query($conn,$query);
 if(mysqli_num_query($run)>0){
+ echo ("Already exist");
     
-$mail = new PHPMailer;
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'd8ac7f95d97bbc';                 // SMTP username
-$mail->Password = '1c06beb3a72dba';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+// $mail = new PHPMailer;
+// $mail->isSMTP();                                      // Set mailer to use SMTP
+// $mail->Host = 'smtp.mailtrap.io';  // Specify main and backup SMTP servers
+// $mail->SMTPAuth = true;                               // Enable SMTP authentication
+// $mail->Username = 'd8ac7f95d97bbc';                 // SMTP username
+// $mail->Password = '1c06beb3a72dba';                           // SMTP password
+// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+// $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('from@example.com', 'Mailer');
-$mail->addAddress($email);     // Add a recipient
+// $mail->setFrom('from@example.com', 'Mailer');
+// $mail->addAddress($email);     // Add a recipient
 
 
-$mail->isHTML(true);                                  // Set email format to HTML
+// $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+// $mail->Subject = 'Here is the subject';
+// $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-    echo("rdtgrd");
-    echo ($email);
-} else {
-    echo 'Message has been sent';
+// if(!$mail->send()) {
+//     echo 'Message could not be sent.';
+//     echo 'Mailer Error: ' . $mail->ErrorInfo;
+//     echo("rdtgrd");
+//     echo ($email);
+// } else {
+//     echo 'Message has been sent';
 
-}
+// }
    
     
     
